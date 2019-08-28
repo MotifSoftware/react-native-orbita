29fccf1bee387eeba6c74b13a23d83449454b5e4
Andrew Merola <andrew@motifsoftware.com>
2019-08-28 10:20:37 -0400

    Changed package name to avoid conflicts

--- android/src/main/java/com/reactlibrary/ONOrbitaModule.java
+++ android/src/main/java/com/reactlibrary/ONOrbitaModule.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.react_native_orbita;
 
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
--- android/src/main/java/com/reactlibrary/ONOrbitaPackage.java
+++ android/src/main/java/com/reactlibrary/ONOrbitaPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.react_native_orbita;
 
 import java.util.Arrays;
 import java.util.Collections;
